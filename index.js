b2e2caadd0ad832614b827ea0ede58692fb37759
piliponful <piliponful@gmail.com>
2020-07-09 22:05:34 +0300

    update cli to match new srpc spec

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ const srpcArguments = process.argv.slice(2)
 const cliName = 'srpc'
 
 const urlId = '--url'
-const functionNameId = '--functionName'
-const functionArgumentsId = '--functionArguments'
+const methodId = '--method'
+const paramsId = '--params'
 
-const cliArgumentsIds = [urlId, functionNameId, functionArgumentsId]
+const cliArgumentsIds = [urlId, methodId, paramsId]
 
 const urlArg = srpcArguments.find(arg => arg.includes(urlId))
-const functionNameArg = srpcArguments.find(arg => arg.includes(functionNameId))
-const functionArgumentsArg = srpcArguments.find(arg => arg.includes(functionArgumentsId))
+const methodArg = srpcArguments.find(arg => arg.includes(methodId))
+const paramsArg = srpcArguments.find(arg => arg.includes(paramsId))
 
-if (!urlArg || !functionNameArg || !functionArgumentsArg) {
+if (!urlArg || !methodArg) {
   console.log(`Usage: ${cliName} ${cliArgumentsIds.join('=something ')}`)
   process.exit(1)
 }
 
 const url = urlArg.replace(`${urlId}=`, '')
-const functionName = functionNameArg.replace(`${functionNameId}=`, '')
-const functionArguments = JSON.parse(functionArgumentsArg.replace(`${functionArgumentsId}=`, ''))
+const method = methodArg.replace(`${methodId}=`, '')
+const params = paramsArg ? JSON.parse(paramsArg.replace(`${paramsId}=`, '')) : {}
 
-fetch(url, { method: 'post', body: JSON.stringify({ functionName, functionArguments }) })
+fetch(url, { method: 'post', body: JSON.stringify({ method, params }) })
   .then(response => response.json())
   .then(console.log)
   .catch(console.error)
